7fed42f05f8e17b1cee752f5d3a57252a47be460
michael-kudrik <mikethesurfer2@gmail.com>
2023-11-27 22:16:04 -0500

    Created Upper Banner, added stylesheets

--- src/main/java/org/mjk/finalproject/Controller1.java
+++ src/main/java/org/mjk/finalproject/Controller1.java
@@ -3,7 +3,7 @@ package org.mjk.finalproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller1 {
     @FXML
     private Label welcomeText;
 
