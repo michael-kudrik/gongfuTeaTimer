36c206dc5b84b5a47cdf9e472f962d1d6fb270bd
michael-kudrik <mikethesurfer2@gmail.com>
2023-11-28 19:45:38 -0500

    Added buttons to right centered anchor pane, styled fxml using css

--- src/main/java/org/mjk/finalproject/App.java
+++ src/main/java/org/mjk/finalproject/App.java
@@ -3,6 +3,7 @@ package org.mjk.finalproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +15,12 @@ public class App extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setMinWidth(800); // Minimum width
+        stage.setMinHeight(450); // Minimum height
+        stage.setMaxWidth(1080); // Maximum width
+        stage.setMaxHeight(600); // Maximum height
         stage.show();
+
     }
 
     public static void main(String[] args) {
