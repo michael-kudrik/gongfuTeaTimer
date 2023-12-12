881774046b06b28b73ee82687bc00382887caabf
michael-kudrik <mikethesurfer2@gmail.com>
2023-12-12 16:20:02 -0500

    added animation, finished the info popup

--- src/main/java/org/mjk/finalproject/App.java
+++ src/main/java/org/mjk/finalproject/App.java
@@ -2,23 +2,25 @@ package org.mjk.finalproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class App extends Application {
+    private static App instance;
+
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-
+        instance = this;
         Image appIcon = new Image(getClass().getResourceAsStream("/org/mjk/finalproject/mugPNG.png"));
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Tea Timer");
@@ -31,13 +33,6 @@ public class App extends Application {
         stage.show();
 
 
-
-
-    }
-
-
-    public static void main(String[] args) {
-        launch();
     }
 
 }
--- src/main/java/org/mjk/finalproject/Controller1.java
+++ src/main/java/org/mjk/finalproject/Controller1.java
@@ -1,36 +1,37 @@
 package org.mjk.finalproject;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
+import javafx.animation.*;
 import javafx.fxml.FXML;
-//Imports To Load New Stage
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.SVGPath;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-//Import Audio
+
 import java.io.File;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller1 {
-//              Add the fxml elements to controller file
+    int brewTime = 15; //default total time in seconds
+    //audio
+    File TeaDone = new File("src/main/resources/org/mjk/finalproject/TeaDone.m4a");
+    File TeaWarning = new File("src/main/resources/org/mjk/finalproject/WarningTea.m4a");
+    Media media = new Media(TeaDone.toURI().toString());
+    MediaPlayer mediaPl = new MediaPlayer(media);
+    Media media2 = new Media(TeaWarning.toURI().toString());
+    MediaPlayer mediaPl2 = new MediaPlayer(media2);
+    File teaDoneFile = new File("src/main/resources/org/mjk/finalproject/TeaDone.mp3");
+    File teaWarningFile = new File("src/main/resources/org/mjk/finalproject/WarningTea.mp3");
+    //              Add the fxml elements to controller file
     @FXML
     private Label teaType;
     @FXML
@@ -47,42 +48,27 @@ public class Controller1 {
     private Button skip;
     @FXML
     private Label teaAmount;
-    int brewTime = 15; //default total time in seconds
+    @FXML
+    private SVGPath teaSvg;
     private int timeMilliseconds = brewTime * 1000; // Convert seconds to ms
     private Timeline timeline; //Init timeline for countdown
     private int infusionCounter = 1;
     private int infusions;
     private int nextInfusionDuration;
-
     //Add borderpane functionality (drag, pressed)
     private double borderX = 0;
     private double borderY = 0;
 
-    //audio
-
-
-    File TeaDone = new File("src/main/resources/org/mjk/finalproject/TeaDone.m4a");
-    File TeaWarning = new File("src/main/resources/org/mjk/finalproject/WarningTea.m4a");
-    Media media = new Media(TeaDone.toURI().toString());
-    Media media2 = new Media(TeaWarning.toURI().toString());
-    MediaPlayer mediaPl = new MediaPlayer(media);
-    MediaPlayer mediaPl2 = new MediaPlayer(media2);
-
-
-
-    File teaDoneFile = new File("src/main/resources/org/mjk/finalproject/TeaDone.mp3");
-    File teaWarningFile = new File("src/main/resources/org/mjk/finalproject/WarningTea.mp3");
-
     //methods
     @FXML
-    private void borderpane_dragged(MouseEvent event){ //Move borderpane when dragged
+    private void borderpane_dragged(MouseEvent event) { //Move borderpane when dragged
         Stage stage = (Stage) borderpane.getScene().getWindow();
         stage.setY(event.getScreenY() - borderY);
         stage.setX(event.getScreenX() - borderX);
     }
 
     @FXML
-    private void borderpane_pressed(MouseEvent event){ //sets starting point based off mouseclick
+    private void borderpane_pressed(MouseEvent event) { //sets starting point based off mouseclick
         borderX = event.getSceneX();
         borderY = event.getSceneY();
     }
@@ -92,6 +78,7 @@ public class Controller1 {
         Stage stage = (Stage) borderpane.getScene().getWindow();
         stage.close();
     }                                   //These SVGs are hard to press, might want to use imageview or put a square ontop of
+
     public void minimizeApp(MouseEvent mouseEvent) {
         Stage stage = (Stage) borderpane.getScene().getWindow();
         stage.setIconified(true);
@@ -151,10 +138,10 @@ public class Controller1 {
 
     //Previous and Skip Functionality
     @FXML
-    private void handlePrevious(){
-        if( infusionCounter > 1){ //I am choosing to allow the user to oversteep the tea, as I sometimes do the same
+    private void handlePrevious() {
+        if (infusionCounter > 1) { //I am choosing to allow the user to oversteep the tea, as I sometimes do the same
             infusionCounter--;
-            infusionCounts.setText(("Infusions: ")+ infusionCounter + " / " + infusions);
+            infusionCounts.setText(("Infusions: ") + infusionCounter + " / " + infusions);
 
             System.out.println("Before Subtraction - brewTime: " + brewTime + ", nextInfusionDuration: " + nextInfusionDuration);
             brewTime -= nextInfusionDuration; // Subtraction here
@@ -164,11 +151,12 @@ public class Controller1 {
         }
 
     }
+
     @FXML
-    private void handleSkip(){
-        if( infusionCounter >= 1){ //I am choosing to allow the user to oversteep the tea, as I sometimes do the same
+    private void handleSkip() {
+        if (infusionCounter >= 1) { //I am choosing to allow the user to oversteep the tea, as I sometimes do the same
             infusionCounter++;
-            infusionCounts.setText(("Infusions: ")+ infusionCounter + " / " + infusions);
+            infusionCounts.setText(("Infusions: ") + infusionCounter + " / " + infusions);
 
             System.out.println("Before Subtraction - brewTime: " + brewTime + ", nextInfusionDuration: " + nextInfusionDuration);
             brewTime += nextInfusionDuration; // Subtraction here
@@ -178,21 +166,16 @@ public class Controller1 {
         }
     }
 
-
     //logic for pressing buttons
     private void updateTeaInfo(String teaName, int firstInfusion, int nextInfusion, int infusions, String amounts) {  //represents grams needed
         //Check if timer is still running and stop it if it is
-       this.infusions = infusions;
+        this.infusions = infusions;
         if (timeline != null && timeline.getStatus() == Animation.Status.RUNNING) {
             timeline.stop();
-           // STARTbtn.setText("Start");
-         //   teaType.setText("00:00:000");
         }
 
         STARTbtn.setText("Start");
         teaType.setText("00:00:000");
-
-
 
         // Update the teaTypeLabel to display the selected tea type
         typeLabel.setText(teaName);
@@ -200,94 +183,101 @@ public class Controller1 {
         teaAmount.setText(amounts);
         nextInfusionDuration = nextInfusion; //this allows the variable to be used outside of this scope
         infusionCounter = 1;
-        infusionCounts.setText(("Infusions: ")+ infusionCounter + " / " + infusions);
+        infusionCounts.setText(("Infusions: ") + infusionCounter + " / " + infusions);
 
         timeMilliseconds = brewTime * 1000; //Resets time allowing user to change teaType while paused
     }
 
-
-        @FXML
-        private void handleStartButton (){ // this comes from the onAction of the buttons fxml
-            if (timeline == null) {
-                startTimer(); //start the timer because not alr running
+    @FXML
+    private void handleStartButton() { // this comes from the onAction of the buttons fxml
+        if (timeline == null) {
+            startTimer(); //start the timer because not alr running
+            STARTbtn.setText("Pause");
+        } else {
+            if (timeline.getStatus() == Animation.Status.RUNNING) { //if running and pressed change to resume
+                timeline.pause();
+                STARTbtn.setText("Resume");
+            } else { // if not running resume and change to pause
+                timeline.play();
                 STARTbtn.setText("Pause");
-            } else {
-                if (timeline.getStatus() == Animation.Status.RUNNING) { //if running and pressed change to resume
-                    timeline.pause();
-                    STARTbtn.setText("Resume");
-                } else { // if not running resume and change to pause
-                    timeline.play();
-                    STARTbtn.setText("Pause");
-                }
-            }
-
-
-        }
-
-        private void startTimer() {
-            System.out.println("Timer started");
-            timeline = new Timeline(new KeyFrame(Duration.millis(1), e -> {
-                timeMilliseconds--;
-                int minutes = timeMilliseconds / (60 * 1000);
-                int seconds = (timeMilliseconds % (60 * 1000)) / 1000;
-                int milliseconds = timeMilliseconds % 1000;
-                String timerDisplay = String.format("%02d:%02d:%03d", minutes, seconds, milliseconds);
-              //DEBUG  System.out.println(timerDisplay);
-                teaType.setText(timerDisplay); //Format the label
-                if (timeMilliseconds <= 0) { //  Add logic for when the timer reaches 0
-                    timeline.stop();
-                    mediaPl.seek(Duration.ZERO); // this pretty much resets to start
-                    mediaPl.play();
-                    STARTbtn.setText("Start"); // Change button back to start
-
-
-
-                    //increment infusions
-                    infusionCounter++;
-                    int infusions = Integer.parseInt(infusionCounts.getText().split(" / ")[1]);
-                    infusionCounts.setText("Infusions: " + infusionCounter + " / " + infusions);
-
-                    //update brewtime
-                    if(infusionCounter <= infusions){
-                        brewTime += nextInfusionDuration;
-                    }
-
-
-                    timeMilliseconds = brewTime * 1000; // Reset timeMilliseconds for the new brewTime
-                }
-                if(timeMilliseconds == 5000){
-                    mediaPl2.seek(Duration.ZERO); // this pretty much resets to start
-                    mediaPl2.play();
-                }
-            }));
-            timeline.setCycleCount(brewTime * 1000); // Repeat indefinitely
-            timeline.play();
-
-        }
-
-        //POPUP From Info Icon
-        @FXML
-        private void handlePopup() {
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("popup.fxml"));
-                Parent root = fxmlLoader.load();
-                Stage popupStage = new Stage();
-                popupStage.initModality(Modality.APPLICATION_MODAL); // Make the popup modal
-                popupStage.setTitle("Info");
-                popupStage.initModality(Modality.APPLICATION_MODAL);
-                popupStage.setScene(new Scene(root));
-                Image appIcon = new Image(getClass().getResourceAsStream("/org/mjk/finalproject/mugPNG.png"));
-                popupStage.getIcons().add(appIcon);
-                popupStage.setResizable(false);
-                popupStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-                //Check if popupfails (hopefully it won't lol)
             }
         }
 
+    }
+
+    private void startTimer() {
+        System.out.println("Timer started");
+        timeline = new Timeline(new KeyFrame(Duration.millis(1), e -> {
+            timeMilliseconds--;
+            int minutes = timeMilliseconds / (60 * 1000);
+            int seconds = (timeMilliseconds % (60 * 1000)) / 1000;
+            int milliseconds = timeMilliseconds % 1000;
+            String timerDisplay = String.format("%02d:%02d:%03d", minutes, seconds, milliseconds);
+            //DEBUG  System.out.println(timerDisplay);
+            teaType.setText(timerDisplay); //Format the label
+            if (timeMilliseconds <= 0) { //  Add logic for when the timer reaches 0
+                timeline.stop();
+                mediaPl.seek(Duration.ZERO); // this pretty much resets to start
+                mediaPl.play();
+                STARTbtn.setText("Start"); // Change button back to start
+                applyShakeAnimation(teaSvg);
+
+                //increment infusions
+                infusionCounter++;
+                int infusions = Integer.parseInt(infusionCounts.getText().split(" / ")[1]);
+                infusionCounts.setText("Infusions: " + infusionCounter + " / " + infusions);
+
+                //update brewtime
+                if (infusionCounter <= infusions) {
+                    brewTime += nextInfusionDuration;
+                }
+                timeMilliseconds = brewTime * 1000; // Reset timeMilliseconds for the new brewTime
+            }
+            if (timeMilliseconds == 5000) {
+                mediaPl2.seek(Duration.ZERO); // this pretty much resets to start
+                mediaPl2.play();
+            }
+        }));
+        timeline.setCycleCount(brewTime * 1000); // Repeat indefinitely
+        timeline.play();
+    }
+
+    //Animate Tea Icon
+    private void applyShakeAnimation(SVGPath teaSvg) {
+        double originalRotate = teaSvg.getRotate(); // Store the original rotation
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(0.1), teaSvg);
+        rotateTransition.setFromAngle(-5);
+        rotateTransition.setToAngle(5);
+        rotateTransition.setCycleCount(5); // Number of times it shakes
+        rotateTransition.setAutoReverse(true); // Back and forth shake motion
+        rotateTransition.setInterpolator(Interpolator.LINEAR); // Linear interpolation for smooth motion
+        rotateTransition.play();
+        rotateTransition.setOnFinished(event -> {
+            teaSvg.setRotate(originalRotate); // Reset to original rotation
+        });
+    }
 
 
+    //POPUP From Info Icon
+    @FXML
+    private void handlePopup() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("popup.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage popupStage = new Stage();
+            popupStage.initModality(Modality.APPLICATION_MODAL); // Make the popup modal
+            popupStage.setTitle("Info");
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+            popupStage.setScene(new Scene(root));
+            Image appIcon = new Image(getClass().getResourceAsStream("/org/mjk/finalproject/mugPNG.png"));
+            popupStage.getIcons().add(appIcon);
+            popupStage.setResizable(false);
+            popupStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //Check if popupfails (hopefully it won't lol)
+        }
+    }
 
 
 }
